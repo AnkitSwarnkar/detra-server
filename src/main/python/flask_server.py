8cc47b298c9db8d772df615ae3bdbbd48acc8e88
Ankit Swarnkar <Ankit.Swarnkar@kohls.com>
2020-02-15 20:13:30 -0800

    agora UI Push Flask

--- src/main/python/flask_server.py
+++ src/main/python/flask_server.py
@@ -1,13 +1,20 @@
 # flask server code
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory, render_template
 from flask_restful import Resource, Api
 from amadeus import Client, ResponseError
 import config
 import json
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder="static", static_url_path='')
 api = Api(app)
 
+
 # a class for a particular resource : Default
+@app.route('/')
+def hello():
+    data = {'app_id': "1d595486132b4f129d960346a5dfefb2",
+            'channel': "test",
+            'token':"0061d595486132b4f129d960346a5dfefb2IACrK2g+caVtV5Sm9Lz7Y6pUmhu5YpKJTewx/vT9r2CJBgx+f9gAAAAAEADB6e0i4dhJXgEAAQDg2Ele"}
+    return render_template('indexdummy.html', data=data)
 
 
 class Hello(Resource):
@@ -39,7 +46,7 @@ class FlightResult(Resource):
             Find best flight offers from Madrid to New York
             '''
             response = amadeus_client.shopping.flight_offers.get(origin='MAD', destination='NYC', departureDate='2020-06-01')
-            print(type(response.data))
+            print(len(response.data))
             return jsonify({'return': response.data})
         # print(response.data)
         except ResponseError as error:
@@ -54,10 +61,8 @@ class Square(Resource):
 
         return jsonify({'square': num**2})
 
-api.add_resource(Hello, '/')
 api.add_resource(FlightResult, '/FLightStatus')
 api.add_resource(Square, '/square/<int:num>')
 
 if __name__ == '__main__':
-
     app.run(debug=True)
